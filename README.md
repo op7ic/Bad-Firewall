1dc1c8befcfce41fbdecefc891d612ea0b07943e
op7ic <3172590+op7ic@users.noreply.github.com>
2022-05-18 14:51:18 +0300

    refresh

--- README.md
+++ README.md
@@ -7,20 +7,20 @@ This is a simple firewall shield stopping known bad IPs.
 The script will execute the following to get necessary packages installed:
 ```
 apt-get -y update
-apt-get install -y ipset iptables curl
+apt-get install -y ipset iptables curl git wget
 ```
 
 ## Prerequisites for Red Hat/Centos based installations
 The script will execute the following to get necessary packages installed:
 ```
 yum -y update
-yum -y install ipset iptables curl 
+yum -y install ipset iptables curl git wget
 ```
 
 ## Installation
 ```
 git clone https://github.com/op7ic/Bad-Firewall.git
-chmod +x shieldme.sh
+cd Bad-Firewall/ && chmod +x shieldme.sh
 ./shieldme.sh
 ```
 
@@ -29,43 +29,37 @@ chmod +x shieldme.sh
 The following known IP ranges are currently blocked:
 
 - [Alienvault IP Reputation](http://reputation.alienvault.com/reputation.data)
-- [Bad IPs](https://www.badips.com/get/list/any/2)
 - [BBcan177 DNSBL](https://gist.githubusercontent.com/BBcan177/bf29d47ea04391cb3eb0/raw/01757cd346cd6080ce12cbc79c172cd3b585ab04/MS-1)
 - [Blocklist.de Blocklist](https://lists.blocklist.de/lists/all.txt)
 - [Blocklist.de export-all](https://www.blocklist.de/downloads/export-ips_all.txt)
-- [Botvrij.eu - ips](http://www.botvrij.eu/data/ioclist.ip-dst.raw)
-- [Brute Force Blocker](http://danger.rulez.sk/projects/bruteforceblocker/blist.php)
-- [C&C IPs](http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt)
 - [CI Bad Guys](http://cinsscore.com/list/ci-badguys.txt)
-- [CoinBlocker IPs](https://zerodot1.gitlab.io/CoinBlockerLists/MiningServerIPList.txt)
-- [Compromised IPs](https://rules.emergingthreats.net/blockrules/compromised-ips.txt)
+- [Emerging Threats](https://rules.emergingthreats.net/blockrules/compromised-ips.txt)
 - [Darklist](http://www.darklist.de/raw.php)
 - [Dictionary SSH Attacks](http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt)
-- [Dyre Botnet IPs](https://sslbl.abuse.ch/blacklist/dyre_sslipblacklist_aggressive.csv)
-- [malwaredomainlist.com](http://www.malwaredomainlist.com/mdl.php?search=&colsearch=All&quantity=All)
 - [TOR IPs - dan.me.uk](https://www.dan.me.uk/torlist/)
 - [TOR IPs - torproject.org](https://check.torproject.org/exit-addresses)
-- [Feodo Tracker](https://feodotracker.abuse.ch/blocklist/?download=ipblocklist)
+- [Feodo Tracker](https://feodotracker.abuse.ch/downloads/ipblocklist.txt)
 - [Ransomware IP Blocklist](https://ransomwaretracker.abuse.ch/downloads/RW_IPBL.txt)
-- [Zeus IP Blocklist](https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist)
 - [GreenSnow Blacklist](http://blocklist.greensnow.co/greensnow.txt)
-- [IPSpamList](http://www.ipspamlist.com/public_feeds.csv)
-- [Malicious EXE IPs](http://www.urlvir.com/export-ip-addresses/)
 - [Talos IP Blacklist](http://www.talosintelligence.com/documents/ip-blacklist)
 - [VoipBL](http://www.voipbl.org/update/)
 - [Dshield](https://iplists.firehol.org/files/dshield.netset)
-- [FireHol](https://iplists.firehol.org/files/firehol_level1.netset)
+- [Threatview.IO Twitter Feed](https://threatview.io/Downloads/Experimental-IOC-Tweets.txt)
+- [Threatview.IO C2 List](https://threatview.io/Downloads/High-Confidence-CobaltStrike-C2%20-Feeds.txt)
+- [Bruteforce IPs](https://jamesbrine.com.au/csv)
+- [URL abuse.ch IPs](https://urlhaus.abuse.ch/downloads/text/)
+
 
 ## CRON job
 
-In order to auto-update the blocks, copy the following code into /etc/cron.d/update-badfirewall. Once a week should be sufficient but feel free to change this. 
+In order to auto-update the blocks, copy the following code into /etc/cron.d/update-badfirewall or add cron entry for specific user to run the script.  
+
 ```
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-MAILTO=root
-0 0 * * 0      root /tmp/Bad-Firewall/shieldme.sh
+0 0 * * 0      root /home/user/BadFirewall/shieldme.sh
 ```
 
 ## Check for dropped packets
+
 Using iptables, you can check how many packets got dropped using the filters:
 ```
 iptables -L INPUT -v --line-numbers # for IPv4
@@ -75,35 +69,46 @@ ip6tables -L INPUT -v --line-numbers # for IPv6
 The table should look similar to this: 
 
 ```
-Chain INPUT (policy ACCEPT 1548 packets, 70960 bytes)
+Chain INPUT (policy ACCEPT 2111 packets, 126K bytes)
 num   pkts bytes target     prot opt in     out     source               destination
-1        2    80 DROP       all  --  any    any     anywhere             anywhere             match-set firehol src
-2        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set feodo src
-3        6   516 DROP       all  --  any    any     anywhere             anywhere             match-set blocklist2 src
-4        8   488 DROP       all  --  any    any     anywhere             anywhere             match-set blocklist1 src
-5        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set darklist src
-6        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set coinblocker src
-7        4   572 DROP       all  --  any    any     anywhere             anywhere             match-set CI_BAD_GUYS src
-8        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set compromised_ips src
-9        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set VoipBL src
-10       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set Botvrij src
-11       9  1028 DROP       all  --  any    any     anywhere             anywhere             match-set badips src
-12      17  2255 DROP       all  --  any    any     anywhere             anywhere             match-set GreenSnow src
-13       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set Zeus src
-14       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set DNSBL src
-15       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set urlvir src
-16       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set Ransomware_IP src
-17       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set Bruteforceblocker src
-18       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set C2 src
-19       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set Talos src
-20       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set dshield src
-21       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set IPSpamList src
-22       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set malwarelist src
-23       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set agressive src
-24       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set ssh src
-25       5   204 DROP       all  --  any    any     anywhere             anywhere             match-set alienvault src
-26       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set tor-individual-ip2 src
-27       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set tor-individual-ip1 src
+1        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set feodo src
+2        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set DNSBL src
+3       12   668 DROP       all  --  any    any     anywhere             anywhere             match-set GreenSnow src
+4       20  1200 DROP       all  --  any    any     anywhere             anywhere             match-set compromised_ips src
+5        1    40 DROP       all  --  any    any     anywhere             anywhere             match-set darklist src
+6       38  1631 DROP       all  --  any    any     anywhere             anywhere             match-set CI_BAD_GUYS src
+7        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set blocklist3 src
+8        0     0 DROP       all  --  any    any     anywhere             anywhere             match-set blocklist2 src
+9        4   240 DROP       all  --  any    any     anywhere             anywhere             match-set blocklist1 src
+10       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set emergingthreats_compromised src
+11       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set threatview_high_confidence_list src
+12       1    40 DROP       all  --  any    any     anywhere             anywhere             match-set firehol src
+13       2    88 DROP       all  --  any    any     anywhere             anywhere             match-set VoipBL src
+14       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set Talos src
+15       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set dshield src
+16       1    40 DROP       all  --  any    any     anywhere             anywhere             match-set bruteforce-ips src
+17       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set ssh src
+18       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set alienvault src
+19       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set tor-individual-ip2 src
+20       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set tor-individual-ip1 src
+21       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set abusechtracker2 src
+22       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set abusechtracker1 src
+23       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set threatview_twitterfeed src
+24       0     0 DROP       all  --  any    any     anywhere             anywhere             match-set threatview_c2feed src
+```
+
+## Deleting full chain
+
+If you would like to destory the set and all the associated rules, iptables needs to be cleared first, followed by deletion of ipset rules. 
+```
+# Clean iptables list for IPv4 or delete individual rulesets using -D option
+iptables --flush
+
+# Clean iptables list for Ipv6 or delete individual rulesets using -D option
+ip6tables --flush
+
+# Remove all sets from ipset
+ipset list | grep Name | awk -F ": " '{print $2}' | xargs -i ipset destroy {}
 ```
 
 ## Modify the blacklists you want to use
--- shieldme.sh
+++ shieldme.sh
@@ -1,143 +1,158 @@
 #!/bin/bash
 # This script should be deployed the box you want to protect from various attacks
 # Tested on Debian/Centos
-# author: op7ic
+# Author: Jerzy 'Yuri' Kramarz (op7ic) 
 
 # do installation based on which package manager is available.
 if VERB="$( which apt-get )" 2> /dev/null; then
    apt-get -y update
-   apt-get install -y ipset iptables curl 
+   apt-get install -y ipset iptables curl bzip2 wget
 elif VERB="$( which yum )" 2> /dev/null; then
    yum -y update
-   yum -y install ipset iptables curl bzip2
+   yum -y install ipset iptables curl bzip2 wget
 fi
 
-echo [+] creating temp directory
+echo [+] Creating Temp Directory
 
 mkdir /tmp/fw-update
 cd /tmp/fw-update
 
 declare -A array
-array["badips"]="https://www.badips.com/get/list/any/2"
 array["DNSBL"]="https://gist.githubusercontent.com/BBcan177/bf29d47ea04391cb3eb0/raw/01757cd346cd6080ce12cbc79c172cd3b585ab04/MS-1"
 array["blocklist1"]="https://lists.blocklist.de/lists/all.txt"
 array["blocklist2"]="https://www.blocklist.de/downloads/export-ips_all.txt"
-array["Botvrij"]="http://www.botvrij.eu/data/ioclist.ip-dst.raw"
-array["Bruteforceblocker"]="http://danger.rulez.sk/projects/bruteforceblocker/blist.php"
+array["blocklist3"]="https://lists.blocklist.de/lists/bruteforcelogin.txt"
 array["CI_BAD_GUYS"]="http://cinsscore.com/list/ci-badguys.txt"
-array["coinblocker"]="https://zerodot1.gitlab.io/CoinBlockerLists/MiningServerIPList.txt"
 array["compromised_ips"]="https://rules.emergingthreats.net/blockrules/compromised-ips.txt"
 array["feodo"]="https://feodotracker.abuse.ch/blocklist/?download=ipblocklist"
 array["darklist"]="http://www.darklist.de/raw.php"
-array["Ransomware_IP"]="https://ransomwaretracker.abuse.ch/downloads/RW_IPBL.txt"
-array["Zeus"]="https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist"
 array["GreenSnow"]="http://blocklist.greensnow.co/greensnow.txt"
-array["urlvir"]="http://www.urlvir.com/export-ip-addresses/"
 array["VoipBL"]="http://www.voipbl.org/update/"
 array["firehol"]="https://iplists.firehol.org/files/firehol_level1.netset"
+array["emergingthreats_compromised"]="https://rules.emergingthreats.net/blockrules/compromised-ips.txt"
+array["threatview_high_confidence_list"]="https://threatview.io/Downloads/IP-High-Confidence-Feed.txt"
 
-
- for i in "${!array[@]}"
+for i in "${!array[@]}"
   do
-   echo [+] downloading blocks for $i addresses from "${array[$i]}"
-   curl ${array[$i]} | grep -v "\#" | sort | uniq > $i.txt
-  done
+  echo [+] Downloading IPs for current $i blocklist from "${array[$i]}"
+  curl -s -A "Firefox" ${array[$i]} | grep -v "\#" | sort | uniq > $i.txt 2> /dev/null
+done
 
-echo [+] downloading IPs for current C2 from http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt
-curl http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt | awk -F "," '{print $1}' | sort | uniq > C2.txt
+echo [+] Downloading IPs for current dshield blocklist from https://iplists.firehol.org/files/dshield.netset
+curl -s -A "Firefox" https://iplists.firehol.org/files/dshield.netset | grep -v "\#" | sort | uniq  > dshield.txt 2> /dev/null
 
-echo [+] downloading IPs for current dshield from https://iplists.firehol.org/files/dshield.netset
-curl https://iplists.firehol.org/files/dshield.netset | grep -v "\#" | sort | uniq  > dshield.txt
+echo [+] Downloading IPs for current ssh dict attacks blocklist from http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt
+curl -s -A "Firefox" http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt | awk -F ": " '{print $2}'  | sort | uniq > ssh.txt 2> /dev/null
 
-echo [+] downloading IPs for current IPSpamList from http://www.ipspamlist.com/public_feeds.csv
-curl http://www.ipspamlist.com/public_feeds.csv | awk -F "," '{print $3}' | sort | uniq > IPSpamList.txt
+echo [+] Downloading IPs for current alienvault blocklist from http://reputation.alienvault.com/reputation.data
+curl -s -A "Firefox" http://reputation.alienvault.com/reputation.data | awk -F "#" '{print $1}' | sort | uniq > alienvault.txt 2> /dev/null
 
-echo [+] downloading IPs for current malwaredomainlist attacks from http://www.malwaredomainlist.com/mdl.php?search=&colsearch=All&quantity=All
-curl "http://www.malwaredomainlist.com/mdl.php?search=&colsearch=All&quantity=All" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq > malwarelist.txt
+echo [+] Downloading IPs for current tor exit nodes blocklist from https://check.torproject.org/exit-addresses
+curl -s -A "Firefox" https://check.torproject.org/exit-addresses | grep ExitAddress | awk '{print $2}' | sort | uniq > tor_current_nodes.txt 2> /dev/null
 
-echo [+] downloading IPs for current dyre_sslipblacklist attacks from https://sslbl.abuse.ch/blacklist/dyre_sslipblacklist_aggressive.csv
-curl https://sslbl.abuse.ch/blacklist/dyre_sslipblacklist_aggressive.csv | awk -F "," '{print $1}' | grep -v "\#" > agressive.txt
+echo [+] Downloading IPs for tor exit node blocklist from https://www.dan.me.uk/torlist/
+curl -s -A "Firefox" https://www.dan.me.uk/torlist/ | sort | uniq > tor_current_nodes_torlist.txt 2> /dev/null
 
-echo [+] downloading IPs for current ssh dict attacks from  http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt
-curl http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt | awk -F ": " '{print $2}'  | sort | uniq > ssh.txt
+echo [+] Downloading IPs for tor exit node blocklist from http://www.talosintelligence.com/documents/ip-blacklist
+wget -q --user-agent "Firefox" http://www.talosintelligence.com/documents/ip-blacklist -O ip-blacklist.txt && cat ip-blacklist.txt | sort | uniq > Talos.txt 2> /dev/null
 
-echo [+] downloading IPs for current alienvault addresses from http://reputation.alienvault.com/reputation.data
-curl http://reputation.alienvault.com/reputation.data | awk -F "#" '{print $1}' | sort | uniq > alienvault.txt
+echo [+] Downloading IPs for bruteforce hosts blocklist from https://jamesbrine.com.au/csv
+curl -s -A "Firefox" https://jamesbrine.com.au/csv | awk -F "," '{print $1}' | sort | uniq | grep -v ipv4 > bruteforce-ips.txt 2> /dev/null
 
-echo [+] downloading IPs for current tor exit node addresses from https://check.torproject.org/exit-addresses
-curl https://check.torproject.org/exit-addresses | grep ExitAddress | awk '{print $2}' | sort | uniq > tor_current_nodes.txt
+echo [+] Downloading IPs for abuse.ch urls and extra IPs blocklist from https://urlhaus.abuse.ch/downloads/text/
+curl -s -A "Firefox" https://urlhaus.abuse.ch/downloads/text/ | grep -o -e "\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}" | sort | uniq > urlhausabuse-ips.txt 2> /dev/null
 
-echo [+] downloading IPs for tor exit node addresses from https://www.dan.me.uk/torlist/
-curl https://www.dan.me.uk/torlist/ | sort | uniq > tor_current_nodes_torlist.txt
+echo [+] Downloading IPs for abuse.ch blocklist from https://feodotracker.abuse.ch/downloads/ipblocklist.txt
+curl -s -A "Firefox" https://feodotracker.abuse.ch/downloads/ipblocklist.txt | grep -o -e "\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}" | sort | uniq > urlhausabuse-ips2.txt 2> /dev/null
 
-echo [+] downloading IPs for tor exit node addresses from http://www.talosintelligence.com/documents/ip-blacklist
-wget http://www.talosintelligence.com/documents/ip-blacklist -O ip-blacklist.txt && cat ip-blacklist | sort | uniq > Talos.txt
+echo [+] Downloading IPs for threatview.io Twitter List blocklist from https://threatview.io/Downloads/Experimental-IOC-Tweets.txt
+curl -s -A "Firefox" https://threatview.io/Downloads/Experimental-IOC-Tweets.txt | grep -o -e "\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}" | sort | uniq > threatview_twitterfeed_ips.txt 2> /dev/null
+
+echo [+] Downloading IPs for threatview.io C2 List blocklist from https://threatview.io/Downloads/High-Confidence-CobaltStrike-C2%20-Feeds.txt
+curl -s -A "Firefox" https://threatview.io/Downloads/High-Confidence-CobaltStrike-C2%20-Feeds.txt | grep -o -e "\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}" | grep -v "127\.0\.0\.1" | sort | uniq > threatview_c2feed_ips.txt 2> /dev/null
+
 
 echo [+] extracting ipv6 addresses
-cat *.txt | grep -Po '(?<![[:alnum:]]|[[:alnum:]]:)(?:(?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4}|(?:[a-f0-9]{1,4}:){1,6}:(?:[a-f0-9]{1,4}:){0,5}[a-f0-9]{1,4})(?![[:alnum:]]:?)' > ipv6.txt
+cat *.txt | grep -Po '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' > ipv6.txt
 
-echo [+] setting up to list blocks
+echo ===== Setting up to IP blocks =====
 
+echo [+] Setting up blocks for threatview_c2feed from https://threatview.io/Downloads/High-Confidence-CobaltStrike-C2%20-Feeds.txt
+ipset create threatview_c2feed hash:net hashsize 32768 maxelem 9999999
+while read line; do ipset add threatview_c2feed $line; done < threatview_c2feed_ips.txt 2>/dev/null
+iptables -I INPUT -m set --match-set threatview_c2feed src -j DROP
+
+echo [+] Setting up blocks for threatview_twitterfeed from https://threatview.io/Downloads/Experimental-IOC-Tweets.txt
+ipset create threatview_twitterfeed hash:net hashsize 32768 maxelem 9999999
+while read line; do ipset add threatview_twitterfeed $line; done < threatview_twitterfeed_ips.txt 2>/dev/null
+iptables -I INPUT -m set --match-set threatview_twitterfeed src -j DROP
+
+echo [+] Setting up blocks for abusechtracker1 from https://urlhaus.abuse.ch/downloads/text/
+ipset create abusechtracker1 hash:net hashsize 32768 maxelem 9999999
+while read line; do ipset add abusechtracker1 $line; done < urlhausabuse-ips.txt 2>/dev/null
+iptables -I INPUT -m set --match-set abusechtracker1 src -j DROP
+
+echo [+] Setting up blocks for abusechtracker2 from https://feodotracker.abuse.ch/downloads/ipblocklist.txt
+ipset create abusechtracker2 hash:net hashsize 32768 maxelem 9999999
+while read line; do ipset add abusechtracker2 $line; done < urlhausabuse-ips2.txt 2>/dev/null
+iptables -I INPUT -m set --match-set abusechtracker2 src -j DROP
+
+echo [+] Setting up blocks for tor-individual-ip1 from https://check.torproject.org/exit-addresses
 ipset create tor-individual-ip1 hash:net hashsize 32768 maxelem 9999999
-while read line; do ipset add tor-individual-ip1 $line; done < tor_current_nodes.txt
+while read line; do ipset add tor-individual-ip1 $line; done < tor_current_nodes.txt 2>/dev/null
 iptables -I INPUT -m set --match-set tor-individual-ip1 src -j DROP
 
+echo [+] Setting up blocks for tor-individual-ip2 from https://www.dan.me.uk/torlist/
 ipset create tor-individual-ip2 hash:net hashsize 32768 maxelem 9999999
-while read line; do ipset add tor-individual-ip2 $line; done < tor_current_nodes_torlist.txt
+while read line; do ipset add tor-individual-ip2 $line; done < tor_current_nodes_torlist.txt 2>/dev/null
 iptables -I INPUT -m set --match-set tor-individual-ip2 src -j DROP
 
+echo [+] Setting up blocks for alienvault from http://reputation.alienvault.com/reputation.data
 ipset create alienvault hash:net hashsize 32768 maxelem 9999999
-while read line; do ipset add alienvault $line; done < alienvault.txt
+while read line; do ipset add alienvault $line; done < alienvault.txt 2>/dev/null
 iptables -I INPUT -m set --match-set alienvault src -j DROP
 
+echo [+] Setting up blocks for ssh from http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt
 ipset create ssh hash:net hashsize 32768 maxelem 9999999
-while read line; do ipset add ssh $line; done < ssh.txt
+while read line; do ipset add ssh $line; done < ssh.txt 2>/dev/null
 iptables -I INPUT -m set --match-set ssh src -j DROP
 
-ipset create agressive hash:net hashsize 32768 maxelem 16777216
-while read line; do ipset add agressive $line; done < agressive.txt
-iptables -I INPUT -m set --match-set agressive src -j DROP
+echo [+] Setting up blocks for bruteforce-ips from https://jamesbrine.com.au/csv
+ipset create bruteforce-ips hash:net hashsize 32768 maxelem 16777216
+while read line; do ipset add bruteforce-ips $line; done < bruteforce-ips.txt 2>/dev/null
+iptables -I INPUT -m set --match-set bruteforce-ips src -j DROP
 
-ipset create malwarelist hash:net hashsize 32768 maxelem 16777216
-while read line; do ipset add malwarelist $line; done < malwarelist.txt
-iptables -I INPUT -m set --match-set malwarelist src -j DROP
-
-ipset create IPSpamList hash:net hashsize 32768 maxelem 16777216
-while read line; do ipset add IPSpamList $line; done < IPSpamList.txt
-iptables -I INPUT -m set --match-set IPSpamList src -j DROP
-
+echo [+] Setting up blocks for dshield from https://iplists.firehol.org/files/dshield.netset
 ipset create dshield hash:net hashsize 32768 maxelem 16777216
-while read line; do ipset add dshield $line; done < dshield.txt
+while read line; do ipset add dshield $line; done < dshield.txt 2>/dev/null
 iptables -I INPUT -m set --match-set dshield src -j DROP
 
+echo [+] Setting up blocks for Talos from http://www.talosintelligence.com/documents/ip-blacklist
 ipset create Talos hash:net hashsize 32768 maxelem 16777216
-while read line; do ipset add Talos $line; done < Talos.txt
+while read line; do ipset add Talos $line; done < Talos.txt 2>/dev/null
 iptables -I INPUT -m set --match-set Talos src -j DROP
 
-ipset create C2 hash:net hashsize 32768 maxelem 16777216
-while read line; do ipset add C2 $line; done < C2.txt
-iptables -I INPUT -m set --match-set C2 src -j DROP
-
+echo [+] Setting up blocks for IPv6 IPs in all block lists
 ipset create ipv6 hash:net family inet6 hashsize 32768 maxelem 16777216
-while read line; do ipset add ipv6 $line; done < ipv6.txt
+while read line; do ipset add ipv6 $line; done < ipv6.txt 2>/dev/null
 ip6tables -I INPUT -m set --match-set ipv6 src -j DROP
 
 for z in "${!array[@]}"
   do
-   echo [+] setting up blocks for $z from "${array[$z]}"
+   echo [+] Setting up blocks for $z from "${array[$z]}"
    ipset create $z hash:net hashsize 32768 maxelem 999999999
-   while read line; do ipset add $z $line; done < $z.txt
-   iptables -I INPUT -m set --match-set $z src -j DROP
+   while read line; do ipset add $z $line; done < $z.txt 2>/dev/null
+   iptables -I INPUT -m set --match-set $z src -j DROP 2>/dev/null
    rm -f $z.txt
 done
 
-echo [+] removing block lists
+echo [+] Removing temp block lists stored locally
 rm -f *.txt
 
-echo [+] saving full output
+echo [+] Saving full firewall block list to /etc/ipset.conf
 ipset save > /etc/ipset.conf
 
-echo [+] Full list of blocked ranges is in blockedranges.txt
-ipset list > blockedranges.txt
+echo [+] Full list of blocked ranges is in /tmp/fw-update/blockedranges.txt
+ipset list > /tmp/fw-update/blockedranges.txt
 
-#No this script is not smart ... you could do loops but hey ho
+echo [!] Please remove /tmp/fw-update folder if no longer needed
